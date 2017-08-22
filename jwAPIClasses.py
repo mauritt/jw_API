23ec0f940eab5e7eb95f68d15ace0bd8ca52cca8
Michael Auritt <Mauritt@Michaels-MacBook-Air.local>
2017-08-22 14:03:06 -0400

    added remaining API classes

--- jwAPIClasses.py
+++ jwAPIClasses.py
@@ -1,45 +1,126 @@
 API_Classes = {
 
-'videos':{
-    'baseURL': 'videos/',
-    'actions': [
-        'create',
-        'delete',
-        'list',
-        'show',
-        'update',
-        ],
+    'videos':{
+        'baseURL': 'videos/',
+        'actions': [
+            'create',
+            'delete',
+            'list',
+            'show',
+            'update',
+            ],
+        },
+
+    'conversions':{
+        'baseURL': 'videos/conversions/',
+        'actions': [
+            'create',
+            'delete',
+            'list',
+            'show',
+            ],
+        },
+
+    'tags':{
+        'baseURL': 'videos/tags/',
+        'actions': [
+            'list',
+            ],
+        },
+
+    'thumbnails':{
+        'baseURL': 'videos/thumbnails/',
+        'actions': [
+            'show',
+            'update',
+            ],
+        },
+
+    'tracks':{
+        'baseURL': 'videos/tracks/',
+        'actions': [
+            'create',
+            'delete',
+            'list',
+            'show',
+            'update',
+            ],
+        },
+
+    'accounts':{
+        'baseURL':'accounts/',
+        'actions': [
+            'show',
+            'update',
+            ],
+        },
+
+    'account-tags':{
+        'baseURL':'accounts/tags/',
+        'actions': [
+            'create',
+            'delete',
+            'list',
+            'show',
+            'update',
+            ],
+        },
+
+    'templates':{
+        'baseURL':'accounts/templates/',
+        'actions': [
+            'create',
+            'delete',
+            'list',
+            'show',
+            'update',
+            ],
+        },
+
+    'usage':{
+        'baseURL':'accounts/usage/',
+        'actions': [
+            'list',
+            'show',
+            ],
+        },
+
+    'channels':{
+        'baseURL':'channels/',
+        'actions': [
+            'create',
+            'delete',
+            'list',
+            'show',
+            'update',
+            ],
+        },
+
+    'channels-videos':{
+        'baseURL':'channels/videos/',
+        'actions': [
+            'create',
+            'delete',
+            'list',
+            'show',
+            'update',
+            ],
+        },
+
+    'players':{
+        'baseURL':'players/',
+        'actions': [
+            'create',
+            'delete',
+            'list',
+            'show',
+            'update',
+            ],
     },
-'conversions':{
-    'baseURL': 'videos/conversions/',
-    'actions': [
-        'create',
-        'delete',
-        'list',
-        'show',
-        ],
-    },
-'tags':{
-    'baseURL': 'videos/tags/',
-    'actions': [
-        'list',
-        ],
-    },
-'thumbnails':{
-    'baseURL': 'videos/thumbnails/',
-    'actions': [
-        'show',
-        'update',
-        ],
-    },
-'tracks':{
-    'baseURL': 'videos/tracks/',
-    'actions': [
-        'create',
-        'delete',
-        'list',
-        'show',
-        'update',
-        ],
-    }
+
+    'status':{
+        'baseURL': 'status/',
+        'actions': [
+            ],
+        },
 }
