c9d627d6f0636f8dd216a31b0159e517bd638602
Michael Auritt <michael@michaelauritt.com>
2017-08-24 09:42:15 -0400

    Added Account method for producing a URL for a video's thumbnail

--- jwCaller.py
+++ jwCaller.py
@@ -11,20 +11,38 @@ import time
 
 class Account:
 
-	def __init__(self, key, secret):
-		self.key = key
-		self.secret = secret
+    delivery_URL = "https://cdn.jwplayer.com"
 
-		self.controller = Controller(self.key, self.secret)
+    def __init__(self, key, secret):
 
-		self.videos = self.controller.API_class('videos')
-		self.conversions = self.controller.API_class('conversions')
-		self.tags = self.controller.API_class('tags')
-		self.tracks = self.controller.API_class('tracks')
-		self.thumbnails = self.controller.API_class('thumbnails')
+        self.key = key
+        self.secret = secret
 
 
+        self.controller = Controller(self.key, self.secret)
 
+        self.videos = self.controller.API_class('videos')
+        self.conversions = self.controller.API_class('conversions')
+        self.tags = self.controller.API_class('tags')
+        self.tracks = self.controller.API_class('tracks')
+        self.thumbnails = self.controller.API_class('thumbnails')
 
-if __name__ == '__main__':
+    def get_thumbnail(self, video_key, thumb_width='720'):
+        allowed_widths = [
+            '40',
+            '120',
+            '320',
+            '480',
+            '640',
+            '720',
+            '1280'
+            '1920'
+            ]
+
+        if thumb_width not in allowed_widths:
+            thumb_width = '720'
+
+        thumb_URL = "{}/thumbs/{}-{}.jpg"
+        return thumb_URL.format(self.delivery_URL, video_key, thumb_width)
+
 
