0e468ff6114d8bb7e39637a2fa3434ab3a09f8bd
Michael Auritt <michael@michaelauritt.com>
2017-08-28 10:04:42 -0400

    Added error handling class and rate limiting

--- jwController.py
+++ jwController.py
@@ -1,63 +1,112 @@
 from jwAPI import jwAPI
 from jwAPIClasses import API_Classes
 import json
-
-class API_class:
-
-    def __init__(self, baseURL, allowed_actions, API):
-
-        self.allowed_actions = allowed_actions
-        self.baseURL = baseURL
-        self.API = API
-
-
-        if "list" in allowed_actions:
-            self.list = API_action(baseURL, 'list', API)
-
-        if "show" in allowed_actions:
-            self.show = API_action(baseURL, 'show', API)
-
-        if "create" in allowed_actions:
-            self.create = API_action(baseURL, 'create', API)
-
-        if "delete" in allowed_actions:
-            self.delete = API_action(baseURL, 'delete', API)
-
-        if "update" in allowed_actions:
-            self.update = API_action(baseURL, 'update', API)
-
-
-
-class API_action:
-
-    def __init__(self, baseURL, action, API):
-        self.API = API
-        self.action = action
-        self.baseURL = baseURL
-
-    def __call__(self, file=False, **params):
-
-
-        URL = "{}{}".format(self.baseURL, self.action)
-
-        req = self.API.call(URL, **params)
-
-        res =  json.loads(req)
-
-        if file and 'file_name' in params.keys():
-            res = self.API.upload(res, file)
-
-        return res
+from time import time, sleep
 
 
 class Controller:
 
+    remaining_calls = 60
+    reset = time()
+
+
     def __init__(self, key, secret):
         self.API = jwAPI(key, secret)
 
-        self.videos = API_class('videos/',API_Classes['videos']['actions'], self.API)
-        self.thumbnails = API_class('videos/thumbnails',API_Classes['thumbnails']['actions'], self.API)
-        self.tags = API_class('videos/tags',API_Classes['tags']['actions'], self.API)
-        self.conversions = API_class('videos/conversions',API_Classes['conversions']['actions'], self.API)
-        self.tracks = API_class('videos/tracks',API_Classes['tracks']['actions'], self.API)
-        self.players = API_class('players/',API_Classes['players']['actions'], self.API)
+        self.videos = self.API_class('videos/',API_Classes['videos']['actions'], self)
+        self.thumbnails = self.API_class('videos/thumbnails',API_Classes['thumbnails']['actions'], self)
+        self.tags = self.API_class('videos/tags',API_Classes['tags']['actions'], self)
+        self.conversions = self.API_class('videos/conversions',API_Classes['conversions']['actions'], self)
+        self.tracks = self.API_class('videos/tracks',API_Classes['tracks']['actions'], self)
+        self.players = self.API_class('players/',API_Classes['players']['actions'], self)
+
+
+    class API_class:
+
+        def __init__(self, baseURL, allowed_actions, controller):
+
+            self.allowed_actions = allowed_actions
+            self.baseURL = baseURL
+            self.API = controller.API
+            self.action = controller.API_action
+            self.errorHandler = controller.errorHandler
+
+
+            if "list" in allowed_actions:
+                self.list = self.errorHandler(self.action(baseURL, 'list', self.API), controller)
+
+            if "show" in allowed_actions:
+                self.show = self.action(baseURL, 'show', self.API)
+
+            if "create" in allowed_actions:
+                self.create = self.action(baseURL, 'create', self.API)
+
+            if "delete" in allowed_actions:
+                self.delete = self.action(baseURL, 'delete', self.API)
+
+            if "update" in allowed_actions:
+                self.update = self.action(baseURL, 'update', self.API)
+
+    class API_action:
+
+        def __init__(self, baseURL, action, API):
+            self.API = API
+            self.action = action
+            self.baseURL = baseURL
+
+        def __call__(self, file=False, **params):
+
+
+            URL = "{}{}".format(self.baseURL, self.action)
+
+            req = self.API.call(URL, **params)
+
+            res =  json.loads(req)
+
+            if file and 'file_name' in params.keys():
+                res = self.API.upload(res, file)
+
+            return res
+
+
+    class errorHandler:
+
+        def __init__(self, API_Class, controller):
+            self.API_Class = API_Class
+            self.controller = controller
+
+        def __call__(self, *argv, **kwargs):
+            self.rate_limiter()
+            resp = self.API_Class(*argv,**kwargs)
+            self.rate_recorder(resp)
+            return resp
+
+        def rate_limiter(self):
+            if self.controller.remaining_calls == 0 and self.controller.reset > int(time()):
+                sleep_time = self.controller.reset - int(time())
+                sleep(sleep_time)
+                print('Sleeping: ' + str(sleep_time))
+
+        def rate_recorder(self, resp):
+            self.controller.remaining_calls = resp['rate_limit']['remaining']
+            self.controller.reset = resp['rate_limit']['reset']
+
+            return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
